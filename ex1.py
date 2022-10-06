345fef555cbf986ab520ec9b7a1d4e0d9c9a4895
JPlay42 <kuruch2004@gmail.com>
2022-10-06 22:03:21 +0300

    correct ex1

--- ex1.py
+++ ex1.py
@@ -19,12 +19,16 @@ class Rectangle:
 
     @width.setter
     def width(self, width: float):
+        if not isinstance(width, float):
+            raise TypeError('Width should be float')
         if width < 0 or width > 20:
             raise ValueError('Width should be from 0 to 20')
         self.__width = width
 
     @length.setter
     def length(self, length: float):
+        if not isinstance(length, float):
+            raise TypeError('Length should be float')
         if length < 0 or length > 20:
             raise ValueError('Length should be from 0 to 20')
         self.__length = length
--- test/test_ex1.py
+++ test/test_ex1.py
@@ -5,32 +5,44 @@ from ex1 import Rectangle
 
 class TestEx1(unittest.TestCase):
     def test_perimeter(self):
-        rectangle = Rectangle(3, 5)
-        self.assertEqual(16, rectangle.perimeter())
+        rectangle = Rectangle(3., 5.)
+        self.assertEqual(16., rectangle.perimeter())
+
+    def test_input_types(self):
+        rectangle = Rectangle(0., 0.)
+        with self.assertRaises(TypeError):
+            rectangle.width = 10
+        with self.assertRaises(TypeError):
+            rectangle.length = 10
+        with self.assertRaises(TypeError):
+            Rectangle(0, 0.)
+        with self.assertRaises(TypeError):
+            Rectangle(0., 0)
+
 
     def test_set_width_limits(self):
-        rectangle = Rectangle(0, 0)
+        rectangle = Rectangle(0., 0.)
         with self.assertRaises(ValueError):
-            rectangle.width = -1
+            rectangle.width = -1.
         with self.assertRaises(ValueError):
-            rectangle.width = 21
+            rectangle.width = 21.
 
     def test_set_length_limits(self):
-        rectangle = Rectangle(0, 0)
+        rectangle = Rectangle(0., 0.)
         with self.assertRaises(ValueError):
-            rectangle.length = -1
+            rectangle.length = -1.
         with self.assertRaises(ValueError):
-            rectangle.length = 21
+            rectangle.length = 21.
 
     def test_constructor_limits(self):
         with self.assertRaises(ValueError):
-            Rectangle(-1, 0)
+            Rectangle(-1., 0.)
         with self.assertRaises(ValueError):
-            Rectangle(21, 0)
+            Rectangle(21., 0.)
         with self.assertRaises(ValueError):
-            Rectangle(0, -1)
+            Rectangle(0., -1.)
         with self.assertRaises(ValueError):
-            Rectangle(0, 21)
+            Rectangle(0., 21.)
 
 
 if __name__ == '__main__':
